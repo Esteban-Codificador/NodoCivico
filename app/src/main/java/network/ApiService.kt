0526517122ff7c9852067476eb845330ca9e8bae
arianaduran25 <arianaduranaadg@gmail.com>
2026-05-29 17:39:51 -0500

    Add API REST integration with Retrofit

---
+++ app/src/main/java/network/ApiService.kt
@@ -0,0 +1,10 @@
+package com.nodocivico.app.network
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiService {
+
+    @GET("posts")
+    fun getPosts(): Call<List<Post>>
+}
---
+++ app/src/main/java/network/Post.kt
@@ -0,0 +1,6 @@
+package com.nodocivico.app.network
+
+data class Post(
+    val id: Int,
+    val title: String
+)
